8bcc25a2bae4603cb1c8db31c725a3dda2fd3313
djazaj <kazakd@mail.ru>
2022-12-12 16:20:36 +0300

    Initial Commit

---
+++ RebaseGitFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RebaseGitFlow
+//
+//  Created by Dmitry Kazak on 12.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
